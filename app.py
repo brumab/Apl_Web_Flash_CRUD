bc8217bc293a65fe7d60df0cb46392ddc87af93b
brumab <brunomolinasouza1986@gmail.com>
2025-12-28 13:22:36 -0300

    fix: replace mysqlclient with pymysql for aiven 6

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-import os
 import pymysql
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mysqldb import MySQL
-from dotenv import load_dotenv
-
-pymysql.install_as_MySQLdb()
-load_dotenv()
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "flash-crud-secret")
 
-# =========================
-# MySQL - AIVEN
-# =========================
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['MYSQL_SSL'] = {'ssl': {}}
+def get_db_connection():
+    return pymysql.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DB"),
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 mysql = MySQL(app)
 
